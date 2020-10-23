fd73541f93fac4fb89da98bcf29c0a3eaa8b6470
vhaibl <44583314+vhaibl@users.noreply.github.com>
2020-10-23 08:55:53 +0300

    some code cleaning

--- randomfive/views.py
+++ randomfive/views.py
@@ -1,48 +1,41 @@
 import requests
 import vk_api
-
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
 from randomfive.forms import NameForm
 
-
 client_id = '7637878'
 client_secret = 'qw3PXN0wzJGwAbXLlRrO'
-redirect_uri = 'http://127.0.0.1/auth'
+redirect_uri = 'http://127.0.0.1:8000/auth'
 
 
 def auth(request):
     if request.method == 'GET':
-        if 'code' in request.GET:
+        if 'code' in request.GET:  # 'code' in request means Oauth selected, obtaining token from VK with Oauth
             code = request.GET['code']
-
             query = f'https://oauth.vk.com/access_token?client_id={client_id}&client_secret={client_secret}' \
-                    f'&redirect_uri=http://127.0.0.1:8000/auth&code={code} '
+                    f'&redirect_uri={redirect_uri}&code={code} '
             response = requests.get(query)
             token = response.json()['access_token']
-            login = response.json()['user_id']
             html = HttpResponse('Password OK, redirecting<meta http-equiv="refresh" content="1;URL=/" />')
-            html.set_cookie('randomfive', token)
-            html.set_cookie('login', login)
+            html.set_cookie('randomfive', token)  # setting COOKIE with token  (logging in)
             return html
 
         form = NameForm()
-        return render(request, 'randomfive/template.html', {'form': form})
+        return render(request, 'randomfive/auth.html', {'form': form, 'client_id': client_id, 'host': redirect_uri})
 
-    if request.method == 'POST':
+    if request.method == 'POST':  # post method when login\pass form filled, obtaining token with VK_api
         form = NameForm(request.POST)
         if form.is_valid():
             login = form.cleaned_data['login']
             password = form.cleaned_data['password']
-            VK = vk_api.VkApi(login, password)
+            vk = vk_api.VkApi(login, password)
             try:
-                VK.auth()
-                token = VK.token['access_token']
-                login = VK.login
+                vk.auth()
+                token = vk.token['access_token']
                 html = HttpResponse('Password OK, redirecting<meta http-equiv="refresh" content="1;URL=/" />')
-                html.set_cookie('randomfive', token)
-                html.set_cookie('login', login)
+                html.set_cookie('randomfive', token)  # setting COOKIE with token (logging in)
                 return html
             except vk_api.BadPassword:
                 return HttpResponse("Bad Password")
@@ -50,21 +43,21 @@ def auth(request):
 
 def showfriends(request):
     if request.COOKIES.get('randomfive'):
-        # запрос френдов
-        token = request.COOKIES.get('randomfive')
-        login = request.COOKIES.get('login')
+        token = request.COOKIES.get('randomfive')  # getting token from COOKIE
         vk = vk_api.VkApi(token=token)
         vk = vk.get_api()
+        user = vk.users.get()
+        name = user[0]['first_name'] + ' ' + user[0]['last_name']  # obtaining user name
         vk_data = vk.friends.get(order='random', count=5, fields='nickname, photo')['items']
-        return render(request, 'randomfive/out.html', {'vk_data': vk_data})
+        return render(request, 'randomfive/out.html', {'vk_data': vk_data, 'name': name})
     else:
         return redirect('/auth')
 
 
 def delete_cookie(request):
     if request.COOKIES.get('randomfive'):
-        response = HttpResponse("Logged out")
-        response.delete_cookie('randomfive')
+        response = HttpResponse('Logged out, redirecting<meta http-equiv="refresh" content="1;URL=/auth" />')
+        response.delete_cookie('randomfive')  # deleting COOKIE (logging out)
     else:
         response = HttpResponse("Need to log in before log out")
     return response
